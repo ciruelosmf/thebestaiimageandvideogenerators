482973a440137118665ea2a52d702398f1a47587
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-06-25 19:53:06 -0300

    add g tag to  blog page

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,8 +1,31 @@
 import React from 'react';
-
+import Script from "next/script";
 export default function Blog() {
   return (
+
+
+
+
     <article className="bg-white p-6 rounded-lg shadow">
+            <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
+          page_path: window.location.pathname,
+          
+});
+         `}
+      </Script>
+
+
+
       <h2 className="text-2xl font-semibold mb-4">AI Made Easy: Find the Best Image & Video Generator for Your Needs
 
 </h2>

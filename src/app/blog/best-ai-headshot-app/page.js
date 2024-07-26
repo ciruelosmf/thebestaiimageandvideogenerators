12a027a2cd5c047347a3abb4b4eff22df1981656
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-07-26 00:55:07 -0300

    add

--- src/app/blog/best-ai-headshot-app/page.js
+++ src/app/blog/best-ai-headshot-app/page.js
@@ -63,7 +63,24 @@ The median price for a basic headshot package is around $250, about 8 times more
 While the cost savings are impressive, the real game-changer is convenience. Upload your selfies from your living room on a lazy Sunday, and by afternoon, you could have dozens of professional-quality headshot options to choose from.
           </p>
            
+          <div className="grid grid-cols-2 gap-4">
 
+
+<img
+    src="           https://storage.googleapis.com/headshothq/style/1333086c-268e-46c4-ad23-6210942d84ec.jpg?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production%40stockai-362303.iam.gserviceaccount.com%2F20240720%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240720T020158Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=26c545b0bb640933eb5e84af2016b12a8f75333ad67534f095cbb445fabf1cd713373567120a78aa09046e6e9fb0752ad92b744bf6e63c686f6e9c9c09ecb9a42623dbce2cc37e658475e953f69ef446e6799a06085eed24678f0a7952293ff4532b9ce781d5b502863951c30c9c4f60328546ddc5fb63c5e8c0cca0ed9f2499a4e6ce672f0ce041367feecfb2d72809aa8ff780b7c25b98223befe3b47bc6cd197f7f601eb03142486f1225dec0873174d0c5ed6219338b1166f92d336fd9afa51fab611028c807a3443e546d96363c0016b5513353667b433cbc1aba4c17638d43c06b059b3528699434320681a24e83ae2701be6ca574a76e2e4b298ae26f"
+    width={300}
+    height={300}
+    alt="Writesonic"
+    className="aspect-square object-cover  shadow-lg"
+  />
+  <img
+    src="https://storage.googleapis.com/postcrafts-public-content/headshotpro/reviews/6662d6c81667a62d19833d5e-6662e42adcb33e12239f19ab-thumbnail.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=headshotpro-backend-production%40stockai-362303.iam.gserviceaccount.com%2F20240720%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20240720T020217Z&X-Goog-Expires=518400&X-Goog-SignedHeaders=host&X-Goog-Signature=0e6701450cda6d9307097d5cdbfdf05350a223252116370154f2956a02fba011507635f0f47b02693fbfac7c0134252e4cb24f4f08b05ed43a9a71e2451f8843ce4241f55ef7a2964c28f1dd28c5990b7927e527054f9d38e88db1deca36ac979b4857ceb05f24f0e6a655b6a9648cc4abc701a22a021b03e5b80d93e0e5ce0c941a148a098be81d722b0784ae4c0049494291de3511d0382e4119e645e6404905b0ad8eb01bd79acd7793242b6dd632945826454d83087546c5905a4e5a274d6751dd33e63821fe00acafaccec6ab642683db6bf65aec0b63e58ce479c547a2fe1b12158e71c7d930fcee1a395fc0935701fb2e3d31011d852c06877c071674"
+    width={300}
+    height={300}
+    alt="Writesonic"
+    className="aspect-square object-cover  shadow-lg"
+  />
+</div>
 
           <h2 className="text-2xl font-semibold mt-8 mb-4"> The Best AI Headshot Generator</h2>
 
@@ -76,8 +93,16 @@ Reality Check: These AI-generated headshots are so real, you might start questio
 Strike a Pose: Forget those stiff, awkward AI poses. HeadshotPro's got you covered with a smorgasbord of natural, flattering angles that'll make you look like you were born ready for your close-up.
 Dress to Impress: Here's the kicker - HeadshotPro lets you play virtual dress-up. Yep, you heard that right. It's the only tool we found that lets you pick your outfit. No more "I have nothing to wear" excuses for your LinkedIn profile pic!
 
+
+
 Bottom line? If you want AI headshots that'll make people wonder if you've got a twin working as a model, HeadshotPro is your golden ticket. It's like having a personal photographer, stylist, and Photoshop wizard all rolled into one AI-powered package. Now that's what we call a head shot!
           
+
+
+<Link href="     https://www.headshotpro.com/?via=aiimageandvideogenerators" className="block  " prefetch={false}> 
+</Link>
+
+
           <Link href="/" className="text-sm font-medium hover:text-primary" prefetch={false}>
             Go to Homepage
           </Link>

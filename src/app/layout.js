ef24bcd5538ae32db4fb1f3fe0ea1c59c3f1b821
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-07-26 02:01:16 -0300

    hog

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Script from "next/script";
 import Head from 'next/head';
 const inter = Inter({ subsets: ["latin"] });
-
+import { CSPostHogProvider } from './providers'
 export const metadata = {
   title: "AI Image and Video Generators",
   description: "AI Image and Video Generators is a directory to discover the best tools to create stunning visuals using AI",
@@ -14,15 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-
-
-      <Script id="posthog">
-        {`
-    !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys getNextSurveyStep onSessionId setPersonProperties".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY,{api_host:'https://us.i.posthog.com', person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
-        })
-         `}
-      </Script>
+ 
 
 
 
@@ -45,8 +37,12 @@ export default function RootLayout({ children }) {
       <meta name="saashub-verification" content="71q2g0y0qjqw" />
 
           </head>
+          <CSPostHogProvider>
+      <body    className={inter.className}>  
+        {children}
 
-      <body className={inter.className}>{children}</body>
+      </body>
+      </CSPostHogProvider>
     </html>
   );
 }
